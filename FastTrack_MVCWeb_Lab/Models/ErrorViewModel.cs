77d35c3026f1c16e8e418a6bfe13257fec49b703
ThanhHT18 <thanhht1797@gmail.com>
2023-10-17 16:53:19 +0700

    Add project files.

---
+++ FastTrack_MVCWeb_Lab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FastTrack_MVCWeb_Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
